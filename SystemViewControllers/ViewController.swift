7a2e65ceec14dd75999de2f5dc65988d126d6601
gdm <gdm.mbp@gdmMBP.local>
2019-03-22 22:42:05 -0400

    Commit

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -7,14 +7,110 @@
 //
 
 import UIKit
+import SafariServices
+import MessageUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate {
+  
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        guard let image = imageView.image else {return}
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = sender
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    //import SafariServices
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        if let url = URL(string: "http://developer.apple.com") {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true, completion: nil)
+        }
+    }
+    
+    //conform to protocols UIImagePickerControllerDelegate, UINavigationControllerDelegate
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alertController = UIAlertController(title: "Choose image source", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(cameraAction)
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoAction = UIAlertAction(title: "Photos", style: .default, handler: { (action) in
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(photoAction)
+        }
+        present(alertController, animated: true, completion: nil)
 
+        alertController.popoverPresentationController?.sourceView = sender
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+            dismiss(animated: true, completion: nil)
+        }
+    }
+   
+    //import MessageUI, conform to protocol MFMailComposeViewControllerDelegate
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+        guard MFMailComposeViewController.canSendMail() else {
+            print("Cannot send mail")
+            return
+        }
+        let mailComposer = MFMailComposeViewController()
+        mailComposer.mailComposeDelegate = self
+        
+        mailComposer.setToRecipients(["example@example.com"])
+        mailComposer.setSubject("Look at this")
+        mailComposer.setMessageBody("Hello, this is an email from the app I made.", isHTML: false)
+        present(mailComposer, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // conform to protocol MFMessageComposeViewControllerDelegate
+    @IBAction func messageButtonTapped(_ sender: Any) {
+        guard MFMessageComposeViewController.canSendText() else {
+            print("Cannot send messages")
+            return
+        }
+        let messageComposeVC = MFMessageComposeViewController()
+        messageComposeVC.delegate = self
+        
+        // Configure the fields of the interface.
+        messageComposeVC.recipients = ["4085551212"]
+        messageComposeVC.body = "Hello from California!"
+        
+        // Present the view controller modally.
+        self.present(messageComposeVC, animated: true, completion: nil)
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
